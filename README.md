16f551a11a94deb98f09d8cc45ad6c9db7ac5593
Ritik Chourasiya <ritvikchoure65+git@gmail.com>
2023-04-29 23:05:33 +0530

    check version

--- README.md
+++ README.md
@@ -11,5 +11,5 @@
 
 Crawlyx is an open-source command-line interface (CLI) based web crawler built using Node.js. It is designed to crawl websites and extract useful information like links, images, and text. It is lightweight, fast, and easy to use.
 
-[![GitHub license](https://img.shields.io/github/license/theritikchoure/zen.svg)](https://github.com/theritikchoure/zen/blob/master/LICENSE)
+[![GitHub license](https://img.shields.io/github/license/theritikchoure/crawlyx.svg)](https://github.com/theritikchoure/crawlyx/blob/master/LICENSE)
 [![Maintainer](https://img.shields.io/badge/maintainer-theiritikchoure-green)](https://github.com/theritikchoure)
--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 const { crawlPage } = require('./crawl');
 const { generateReport } = require('./report/report');
+const package = require('./package');
 
 exports.mainFunction = main;
 
 async function main() {
     if(process.argv.length < 3) {
         console.log('no website provided');
-        process.exit(1)
+        process.exit(1);
     }
     
     if(process.argv.length > 3) {
         console.log('too many arguments');
-        process.exit(1)
+        process.exit(1);
     }
 
     const baseUrl = process.argv[2];
+
+    if(baseUrl === '--version' || baseUrl === '--v') {
+        console.log(`> ${package.version}`);
+        process.exit(1);
+    }
 
     console.log(`starting crawl of ${baseUrl}`)
     const pages = await crawlPage(baseUrl, baseUrl, {});
