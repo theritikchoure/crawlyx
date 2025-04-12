ce62cedb79da3f414f78e9a6d4997bbc399986f2
Ritik Chourasiya <ritvikchoure65+git@gmail.com>
2025-04-12 08:47:51 +0530

    fix: Cannot run crawlyx on Ubuntu 22.04

--- main.js
+++ main.js
@@ -10,36 +10,38 @@ exports.mainFunction = main;
 
 // Define the 'main' function, which serves as the entry point for the application.
 async function main() {
-    // Check if there are fewer than 3 command-line arguments.
-    if (process.argv.length < 3) {
-        console.log('no website provided');
-        process.exit(1); // Exit with an error code.
-    }
+  // Check if there are fewer than 3 command-line arguments.
+  if (process.argv.length < 3) {
+    console.log("no website provided");
+    process.exit(1); // Exit with an error code.
+  }
 
-    // Check if there are more than 3 command-line arguments.
-    if (process.argv.length > 3) {
-        console.log('too many arguments');
-        process.exit(1); // Exit with an error code.
-    }
+  // Check if there are more than 3 command-line arguments.
+  if (process.argv.length > 3) {
+    console.log("too many arguments");
+    process.exit(1); // Exit with an error code.
+  }
 
-    // Extract the base URL from the command-line argument, assuming it's a valid URL.
-    const baseUrl = (new URL(process.argv[2])).origin;
+  const argument = process.argv[2];
 
-    // Check if the command-line argument is '--version' or '--v'.
-    if (baseUrl === '--version' || baseUrl === '--v') {
-        console.log(`> ${package.version}`); // Print the package version.
-        process.exit(1); // Exit with an error code.
-    }
+  // Check if the command-line argument is '--version' or '--v'.
+  if (argument === "--version" || argument === "--v") {
+    console.log(`> ${package.version}`); // Print the package version.
+    process.exit(1); // Exit with an error code.
+  }
 
-    // Print a message indicating the start of the crawl for the provided URL.
-    console.log(`starting crawl of ${baseUrl}`);
+  // Extract the base URL from the command-line argument, assuming it's a valid URL.
+  const baseUrl = new URL(process.argv[2]).origin;
 
-    // Call the 'crawlPage' function to crawl the website and retrieve the 'pages' data.
-    const pages = await crawlPage(baseUrl, baseUrl, {});
+  // Print a message indicating the start of the crawl for the provided URL.
+  console.log(`starting crawl of ${baseUrl}`);
 
-    // Generate a report based on the crawled data for the specified 'baseUrl'.
-    generateReport(baseUrl, pages);
+  // Call the 'crawlPage' function to crawl the website and retrieve the 'pages' data.
+  const pages = await crawlPage(baseUrl, baseUrl, {});
+
+  // Generate a report based on the crawled data for the specified 'baseUrl'.
+  generateReport(baseUrl, pages);
 }
 
 // The 'main' function is currently commented out. To execute the code directly from terminal, uncomment the 'main' function.
-// main();
+main();
